599fd9ae3d12591af8b14301678084742e36aa69
TheBlueBaron <ronniehughjames@protonmail.com>
2025-05-03 15:53:16 +0100

    feat(): add navbar

--- job-board/components/general/Navbar.tsx
+++ job-board/components/general/Navbar.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { ThemeToggle } from "./ThemeToggle";
+
 export function Navbar() {
-    
+    return (
+        <nav className="flex items-center justify-between py-5">
+            <Link href={"/"} className="flex items-center gap-2">
+                <Image src={Logo} alt="Logo Next Job" width={80} height={40} />
+                <h1 className="text-2xl font-bold">Next<span className="text-primary">Job</span></h1>
+            </Link>
+
+            <div className="flex items-center gap-4">
+                <ThemeToggle />
+                <Button>Login</Button>
+            </div>
+        </nav>
+    )
 }
