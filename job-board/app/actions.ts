55eb35dcd5cbefcfec608c36b9f3b39e968127a5
TheBlueBaron <ronniehughjames@protonmail.com>
2025-05-27 14:52:32 +0100

    feat(): add arcjet shield and detectbot

--- job-board/app/actions.ts
+++ job-board/app/actions.ts
@@ -5,9 +5,36 @@ import { requireUser } from "./utils/requireUser";
 import { companySchema, jobSeekerSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "./utils/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: "LIVE",
+    }),
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    }),
+  );
+
+async function ajDecision() {
+  const req = await request();
+
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
+}
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const session = await requireUser();
+
+  ajDecision();
 
   const validateData = companySchema.parse(data);
 
@@ -31,6 +58,8 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
 
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const session = await requireUser();
+
+  ajDecision();
 
   const validateData = jobSeekerSchema.parse(data);
 
